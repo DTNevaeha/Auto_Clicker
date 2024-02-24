e32de72139d100e2d87904d6b5bb305ecb59e5ad
DTNevaeha <DTNevaeha@gmail.com>
2024-02-24 16:46:07 -0600

    Clicker now works with left and right click

--- nev_auto_clicker.py
+++ nev_auto_clicker.py
@@ -10,13 +10,10 @@ class AutoClicker:
         self.interval = 1.0
         self.counter = 0
         self.click_thread = None
-        self.button = Button.left  # Initialize the 'button' attribute here
+        self.is_left_click = True
     
-    def toggle_click(self):
-        if self.button == Button.left:
-            self.button = Button.right
-        else:
-            self.button = Button.left
+    def toggle_button(self):
+        self.is_left_click = not self.is_left_click
 
     def toggle_clicking(self):
         if self.clicking:
@@ -31,7 +28,8 @@ class AutoClicker:
     def click_loop(self):
         time.sleep(1) # delay before clicking starts
         while self.clicking:
-            self.mouse.click(self.button, 1)
+            button = Button.left if self.is_left_click else Button.right
+            self.mouse.click(button, 1)
             time.sleep(self.interval)
 
     def set_interval(self, interval):
@@ -43,26 +41,35 @@ class AutoClicker:
 class AutoClickerGUI:
     def __init__(self, root, clicker):
         self.clicker = clicker
+
+        self.button_label = tk.Label(root, text=f"Current Click: {'Left' if self.clicker.is_left_click else 'Right'}")
+        self.button_label.pack()
+
         self.toggle_button = tk.Button(root, text="Start Clicking", command=self.toggle_clicking)
         self.toggle_button.pack()
-        self.counter_button = tk.Button(root, text="Increase Counter", command=self.increase_counter)
-        self.counter_button.pack()
-        self.counter_label = tk.Label(root, text=f"Counter: {self.clicker.counter}")
-        self.counter_label.pack()
+
+        self.click_button = tk.Button(root, text="Toggle to Right Click", command=self.toggle_click_button)
+        self.click_button.pack(pady=20)
+        
+        self.interval_label = tk.Label(root, text=f"Current Click Interval: {self.clicker.interval} seconds")
+        self.interval_label.pack()
+        self.interval_label_2 = tk.Label(root, text=f"Enter New Speed Below")
+        self.interval_label_2.pack()
         self.interval_entry = tk.Entry(root)
         self.interval_entry.pack()
         self.interval_button = tk.Button(root, text="Set Click Interval", command=self.set_interval)
         self.interval_button.pack()
-        self.interval_label = tk.Label(root, text=f"Current Click Interval: {self.clicker.interval} seconds")
-        self.interval_label.pack()
+        
         self.button_click_count = 0
+
+        self.counter_display = tk.Label(root, text=f"Test Click Speed Below")
+        self.counter_display.pack()
+        self.counter_button = tk.Button(root, text="Increase Counter", command=self.increase_counter)
+        self.counter_button.pack()
+        self.counter_label = tk.Label(root, text=f"Counter: {self.clicker.counter}")
+        self.counter_label.pack()
+
         root.protocol("WM_DELETE_WINDOW", self.on_close)
-
-        self.button_label = tk.Label(root, text=f"Button: {self.clicker.button}")
-        self.button_label.pack()
-
-        self.click_button = tk.Button(root, text="Left Click", command=self.toggle_button)
-        self.click_button.pack()
 
     def toggle_clicking(self):
         if self.clicker.clicking:
@@ -83,20 +90,17 @@ class AutoClickerGUI:
         except ValueError:
             pass
     
-    def toggle_button(self):
+    def toggle_click_button(self):
         self.clicker.toggle_button()
-        # self.button_label.config(text=f"Button: {self.clicker.button}")
-        if self.clicker.button == Button.left:
-            self.click_button.config(text="Right Click")
-        else:
-            self.click_button.config(text="Left Click")
+        self.button_label.config(text=f"Current Click: {'Left' if self.clicker.is_left_click else 'Right'}")
+        self.click_button.config(text=f"Toggle to {'Right' if self.clicker.is_left_click else 'Left'} Click")
 
     def on_close(self):
         self.clicker.stop_clicking()
         root.destroy()
 
 root = tk.Tk()
-root.geometry("300x200")  # Set the default size of the window to 500x500 pixels
+root.geometry("300x400")  # Set the default size of the window to 500x500 pixels
 clicker = AutoClicker()
 gui = AutoClickerGUI(root, clicker)
 root.mainloop()
